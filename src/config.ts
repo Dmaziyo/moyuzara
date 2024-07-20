b798a2ab1bc305fcab9046d91517abaa3c6b885b
liangjunqi <247948681@qq.com>
2024-07-20 15:23:13 +0800

    feat: first post

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE = {
 	keywords: 'Dct,Weekly,Blog,前端,MaZiYo,摸鱼',
 	icon: 'https://raw.githubusercontent.com/Dmaziyo/blog-img/576bdab4da41cee1ab072da3cd108cf1aa6bcf5c/fish.svg',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
-	homePage: 'https://weekly.tw93.fun',
+	homePage: 'https://dmaziyo.github.io/moyuzara',
 	blogPage: 'https://dmaziyo.github.io/',
 	twitterId: 'DctZi',
 	githubId: 'Dmaziyo',
